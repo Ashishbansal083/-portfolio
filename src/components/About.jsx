5e6143dca290c5a7824ac7d256518960be0fe8fd
Ashish <bansalgashi083@gmail.com>
2023-07-20 00:05:53 +0530

    minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 const About = () => {
   return (
     <div name='About' className='w-full bg-gradient-to-b from-gray-800 to-black text-white h-screen'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
-        <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, placeat obcaecati nisi adipisci commodi quis. Aliquid porro quia accusamus neque quisquam eum quaerat voluptatum eos magni tempore, soluta distinctio ducimus ab unde alias est reiciendis, sit repellendus sint dolor quam a, rem minus. Nulla voluptatibus, ratione minima eius reprehenderit voluptatum?</p>
-        <br/>
-        <p className='text-xl'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa voluptatibus, unde perferendis ipsa natus, error quo iste qui nemo, totam possimus obcaecati architecto. Provident rerum, consectetur distinctio obcaecati totam exercitationem aliquid. Eum, corporis! Error, in facere repellendus odio quaerat nulla voluptas minima doloremque tempore ullam animi quidem corporis minus amet.</p>
-        </div>        
+        <p className='text-xl mt-20'>I am an experienced Full Stack Web Developer with a strong skill set in creating responsive web applications using React.js, Node.js, and Git. Known for my troubleshooting abilities and attention to optimization and security, I thrive in collaborating with cross-functional teams and possess excellent end-user support skills. 
+        </p>
+        <br />
+        <p className='text-xl'>As a successful freelancer on UpWork, I have utilized my business acumen and client understanding to deliver customized solutions that consistently exceed expectations. My dedication lies in enhancing website functionality and user experience through continuous development and maintenance.</p>
+      </div>
     </div>
   )
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,36 +1,39 @@
 import React from 'react'
-import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
-import installNode from '../assets/portfolio/installNode.jpg'
+import Recipesearch from '../assets/portfolio/Recipesearch .png'
+import TodoList from '../assets/portfolio/TodoList.png'
 import navbar from '../assets/portfolio/navbar.jpg'
-import reactParallax from '../assets/portfolio/reactParallax.jpg'
+import ecommerce from '../assets/portfolio/ecommerce.png'
 import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
 import reactWeather from '../assets/portfolio/reactWeather.jpg'
 
 const Portfolio = () => {
-  const portfolios=[
+  const portfolios = [
     {
-      id:1,
-      src:arrayDestruct,
+      id: 1,
+      src: Recipesearch,
+      url: 'https://search-recipe-fe76b.web.app/'
     },
     {
-      id:2,
-      src:reactParallax,
+      id: 2,
+      src: ecommerce,
+      url:'https://ecommerce-9a374.web.app/'
     },
     {
-      id:3,
-      src:installNode,
+      id: 3,
+      src: TodoList,
+      url:'https://todo-list-nu-five.vercel.app/'
     },
     {
-      id:4,
-      src:navbar,
+      id: 4,
+      src: navbar,
     },
     {
-      id:5,
-      src:reactSmooth,
+      id: 5,
+      src: reactSmooth,
     },
     {
-      id:6,
-      src:reactWeather,
+      id: 6,
+      src: reactWeather,
     },
   ]
   return (
@@ -41,15 +44,16 @@ const Portfolio = () => {
           <p className='py-6'>Check out some of my work right here</p>
         </div>
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-          {portfolios.map(({id,src})=>
-          <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
-          <img src={src} alt='image' className='rounded-md duration-200 hover:scale-105'/>
-          <div className='flex items-center justify-center'>
-            <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 '>Demo</button>
-            <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 '>Code</button>
-          </div>
-        </div>
-          )}          
+          {portfolios.map(({ id, src, url }) =>
+            <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>              
+                <img src={src} alt='image' className='rounded-md duration-200 hover:scale-105' />              
+              <div className='flex items-center justify-center '>
+                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 '>
+                  <a href={url} target='blank'>Demo</a></button>
+                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 '>Code</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
